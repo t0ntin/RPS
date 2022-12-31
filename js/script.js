f7f9b041b14f8e0c32b0c4c94257a75ad7d15070
t0ntin <115113636+t0ntin@users.noreply.github.com>
2022-12-31 09:50:37 -0500

    Game was playing for 4 rounds instead of 5
    removed one of the prompts, changed it for an initial confirm prompt. Now it plays 5 rounds.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
-
-
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let computerChoice = randomNumber === 1 ? "rock"
-  : randomNumber === 2 ? "paper"
-  : "scissors";
+  let computerChoice =
+    randomNumber === 1 ? "rock" : randomNumber === 2 ? "paper" : "scissors";
   return computerChoice;
-
 }
 
 function convertToLowerCase(userInput) {
-   var result = userInput.toLowerCase();
-   return result;
+  var result = userInput.toLowerCase();
+  return result;
 }
-
-
 
 function rpsRound(playerSelection, computerChoice) {
   playerSelection = convertToLowerCase(playerSelection);
-  
-  let winner = playerSelection === computerChoice ? "It's a tie!"
-  : playerSelection === "rock" && computerChoice === "paper" ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
-  : playerSelection === "paper" && computerChoice === "scissors" ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
-  : playerSelection === "scissors" && computerChoice === "rock" ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
-  : `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n YOU WIN!!!`
+
+  let winner =
+    playerSelection === computerChoice
+      ? "It's a tie!"
+      : playerSelection === "rock" && computerChoice === "paper"
+      ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
+      : playerSelection === "paper" && computerChoice === "scissors"
+      ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
+      : playerSelection === "scissors" && computerChoice === "rock"
+      ? `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n The computer wins.`
+      : `You chose: ${playerSelection}\n The computer chose ${computerChoice}. \n YOU WIN!!!`;
   console.log(winner);
 
   return winner;
 }
 
 function game() {
-  for (let i = 0; i < 4; i++) {
+  confirm("Let's play Rock Paper Scissors for 5 rounds.");
+  for (let i = 0; i < 5; i++) {
     let computerChoice = getComputerChoice();
-    let playerSelection = prompt("enter rock paper or scissors");
+    let playerSelection = prompt("Enter rock, paper or scissors.");
     rpsRound(playerSelection, computerChoice);
-    if (i === 0) {
-      playerSelection = prompt("let's play 4 more rounds. Enter RPC");
-    }
-
-    
- }
+  }
 }
 
 game();
